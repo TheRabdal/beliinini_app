c681f0a4e302e116e92515ba20b874b407639921
TheRabdal <kautsarrabdal@gmail.com>
2025-12-11 11:56:40 +0700

    fix login_services

--- lib/modules/login/services/login_service.dart
+++ lib/modules/login/services/login_service.dart
@@ -1,11 +1,15 @@
 import 'package:beliinini_app/packages/packages.dart';
 
 class LoginService {
-  Future<LoginResponse> login(String username, String password) async {
+  Future<LoginResponse> login(
+    String username, 
+    String password) async {
     try {
       final response = await DioLogger.instance.post(
         'https://fakestoreapi.com/auth/login',
-        data: {'username': username, 'password': password},
+        data: {
+          'username': username, 
+          'password': password},
       );
 
       if (response.statusCode == 201) {
